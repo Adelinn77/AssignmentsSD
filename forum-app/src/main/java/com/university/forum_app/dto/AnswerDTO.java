ef8d687128da942b6d48fdf072b6aa9e1ea05d59
Denis <popa.fl.denis@student.utcluj.ro>
2026-03-28 19:43:26 +0200

    version2

--- forum-app/src/main/java/com/university/forum_app/dto/AnswerDTO.java
+++ forum-app/src/main/java/com/university/forum_app/dto/AnswerDTO.java
@@ -1,4 +1,22 @@
 package com.university.forum_app.dto;
 
+import com.university.forum_app.entity.Question;
+import com.university.forum_app.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AnswerDTO {
+    private Long answerId;
+    private Long questionId;
+    private Long userId;
+    private LocalDateTime dateTime;
+
 }
