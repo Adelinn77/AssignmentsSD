3ea8316b932cd6665409d509bfde973ff3d71cfd
TudorUrzica <tudurz77@gmail.com>
2026-04-26 04:08:50 +0300

    created_a_userlist_page_in_navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { QuestionList } from './features/questions/pages/question-list/question-list';
 import { Login } from './features/auth/pages/login/login';
 import { UserProfile } from './features/users/pages/user-profile/user-profile';
+import { UserList } from './features/users/pages/user-list/user-list';
 
 export const routes: Routes = [
   { path: 'questions', component: QuestionList },
   { path: 'auth/login', component: Login },
   { path: 'users/profile', component: UserProfile },
+  { path: 'users', component: UserList },
 
   { path: '', redirectTo: '/questions', pathMatch: 'full' },
 
