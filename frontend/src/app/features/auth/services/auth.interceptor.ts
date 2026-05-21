a0102aac8eb1a7318265b25c04e6fa1515772e37
Adelinn77 <adelinvancea2004@gmail.com>
2026-05-21 14:01:03 +0300

    fixed the ordering of q/a, security issue, and questions images rednering

--- frontend/src/app/features/auth/services/auth.interceptor.ts
+++ frontend/src/app/features/auth/services/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('userToken');
+  const token = sessionStorage.getItem('userToken');
 
   if (token && !req.url.includes('/api/auth/')) {
     const clonedRequest = req.clone({
